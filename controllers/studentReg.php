b9cc69d3fe4cf7414342397b723cb9aab62c2a15
arunbillur <arunbillur@xiaomi.com>
2021-03-18 15:07:43 +0530

    Sage Project

--- controllers/studentReg.php
+++ controllers/studentReg.php
@@ -4,15 +4,18 @@ $controller = new StudentController();
 
 if (isset($_POST) && $_POST['student_type'] == 'registration') {
     $redirect = $controller->studentRegister($_POST);
+    echo "<script type='text/javascript'>alert('Saved successfully!')</script>";
     header('Location: '.$redirect);
 }
 
 if (isset($_POST) && $_POST['student_type'] == 'cource') {
     $redirect = $controller->courceRegister($_POST);
+    echo "<script type='text/javascript'>alert('Saved successfully!')</script>";
     header('Location: '.$redirect);
 }
 
 if (isset($_POST) && $_POST['student_type'] == 'cource_subscribe') {
     $redirect = $controller->courceSubscribe($_POST);
+    echo "<script type='text/javascript'>alert('Saved successfully!')</script>";
     header('Location: '.$redirect);
 }
--- views/studentView.php
+++ views/studentView.php
@@ -192,7 +192,7 @@
         </div>
         <div class="item">
           <p>Phone</p>
-          <input type="number" name="phone" placeholder="### ### ####"/>
+          <input type="number" name="phone" min="10" max = "10" placeholder="### ### ####"/>
         </div>
         <div class="item">
           <p>D O B</p>
@@ -274,7 +274,7 @@
     <input type="date" placeholder="DOB" id = "edob" name="dob" required>
 
     <label for="phone"><b>Phone</b></label>
-    <input type="number" placeholder="Phone" id = "ephone" name="phone" required>
+    <input type="number" placeholder="Phone" id = "ephone" name="phone" min="10" max = "10" required>
 
     <button type="submit" class="btn">Submit</button>
     <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
